fa4e37561908af15e2fc482e7fc6139fbb59968d
Narasimha362 <117163411+Narasimha362@users.noreply.github.com>
2023-01-04 16:43:12 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,32 +1,14 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAQYRHLJBBYSFOS76A"
-secret_key = "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"
+access_key = "AKIAQYRHLJBB2NSAA7OA"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
 ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.micro"
 tags = {
-Name = "Reddy"
-environment = "Dev"
-}
-}
-
-resource "aws_instance" "two" {
-ami = "ami-0cca134ec43cf708f"
-instance_type = "t2.micro"
-tags = {
-Name = "Simha"
-environment = "Prod"
-}
-}
-
-resource "aws_instance" "three" {
-ami = "ami-0cca134ec43cf708f"
-instance_type = "t2.micro"
-tags = {
-Name = "Narasimha"
-environment = "Test"
+Name = "narasimha"
+Environment = "dev"
 }
 }
